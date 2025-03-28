59f6b5d1a2cc6b557ef8d7411e1a4973c7873957
Ali Akbar <sayaliakbar@gmail.com>
2025-03-28 17:51:00 +0500

    Save cart in localstorage

--- data/cart.js
+++ data/cart.js
@@ -1,13 +1,21 @@
-export let cart = [
-  {
-    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity: 2,
-  },
-  {
-    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity: 4,
-  },
-];
+export let cart = JSON.parse(localStorage.getItem("cart"));
+
+if (!cart) {
+  cart = [
+    {
+      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      quantity: 2,
+    },
+    {
+      productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+      quantity: 4,
+    },
+  ];
+}
+
+function saveToLocalStorage() {
+  localStorage.setItem("cart", JSON.stringify(cart));
+}
 
 export function addToCart(productId, addedToCartVar) {
   let matchingcartItem;
@@ -41,6 +49,8 @@ export function addToCart(productId, addedToCartVar) {
       .querySelector(`.js-added-to-cart-${productId}`)
       .classList.remove("added-to-cart-visible");
   }, 2000);
+
+  saveToLocalStorage();
 }
 
 export function removeFromCart(productId) {
@@ -55,4 +65,6 @@ export function removeFromCart(productId) {
   cart = newCart;
 
   document.querySelector(`.js-cart-item-container-${productId}`).remove();
+
+  saveToLocalStorage();
 }
