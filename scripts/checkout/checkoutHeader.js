b7f3dcd52da516056c5580b369d6c4aacdf8a2c5
Ali Akbar <sayaliakbar@gmail.com>
2025-03-29 14:56:39 +0500

    Created this file

---
+++ scripts/checkout/checkoutHeader.js
@@ -0,0 +1,14 @@
+import { cart } from "../../data/cart.js";
+
+function renderCheckoutHeader() {
+  let cartItems = 0;
+  cart.forEach((cartItem) => {
+    cartItems += cartItem.quantity;
+  });
+
+  const html = `Checkout (<a class="return-to-home-link js-cart-items-quantity" href="amazon.html">${cartItems} items</a>)`;
+
+  document.querySelector(".js-checkout-header-middle-section").innerHTML = html;
+}
+
+export default renderCheckoutHeader;
