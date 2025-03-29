e719afef4f594c9257d4337770d9adbe4597040a
Ali Akbar <sayaliakbar@gmail.com>
2025-03-29 10:10:32 +0500

    Created delivery date options

---
+++ data/deliveryOptions.js
@@ -0,0 +1,19 @@
+const deliveryOptions = [
+  {
+    id: "1",
+    dileveryDays: 7,
+    priceCents: 0,
+  },
+  {
+    id: "2",
+    dileveryDays: 3,
+    priceCents: 499,
+  },
+  {
+    id: "3",
+    dileveryDays: 1,
+    priceCents: 999,
+  },
+];
+
+export default deliveryOptions;
