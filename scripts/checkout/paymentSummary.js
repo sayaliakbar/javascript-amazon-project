97cde952e5054aba0630b4a243ba43ff10d6be31
Ali Akbar <sayaliakbar@gmail.com>
2025-03-29 13:22:54 +0500

    Updated the DOM html of payment summary with js

--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -26,7 +26,49 @@ function renderPaymentSummary() {
   const taxCents = totalBeforeTaxCents * 0.1;
   const totalCents = totalBeforeTaxCents + taxCents;
 
-  const paymentSummaryHTML = ``;
+  const paymentSummaryHTML = `<div class="payment-summary-title">
+          Order Summary
+        </div>
+
+        <div class="payment-summary-row">
+          <div>Items (${cartItemQuantity}):</div>
+          <div class="payment-summary-money">$${formatCurrency(
+            productPriceCents
+          )}</div>
+        </div>
+
+        <div class="payment-summary-row">
+          <div>Shipping &amp; handling:</div>
+          <div class="payment-summary-money">$${formatCurrency(
+            shippingPriceCents
+          )}</div>
+        </div>
+
+        <div class="payment-summary-row subtotal-row">
+          <div>Total before tax:</div>
+          <div class="payment-summary-money">$${formatCurrency(
+            totalBeforeTaxCents
+          )}</div>
+        </div>
+
+        <div class="payment-summary-row">
+          <div>Estimated tax (10%):</div>
+          <div class="payment-summary-money">$${formatCurrency(taxCents)}</div>
+        </div>
+
+        <div class="payment-summary-row total-row">
+          <div>Order total:</div>
+          <div class="payment-summary-money">$${formatCurrency(
+            totalCents
+          )}</div>
+        </div>
+
+        <button class="place-order-button button-primary">
+          Place your order
+        </button>
+      </div>`;
+
+  document.querySelector(".js-payment-summary").innerHTML = paymentSummaryHTML;
 }
 
 export default renderPaymentSummary;
