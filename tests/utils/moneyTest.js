b717d44adb99a008758ce74880793783879711ed
Ali Akbar <sayaliakbar@gmail.com>
2025-03-30 17:38:15 +0500

    Refactor currency formatting tests for improved organization and clarity

--- tests/utils/moneyTest.js
+++ tests/utils/moneyTest.js
@@ -1,23 +1,25 @@
 import { formatCurrency } from "../../utils/money.js";
 
-describe("test suite: formatCurrency", () => {
-  it("converts cents into dollors", () => {
-    expect(formatCurrency(2095)).toEqual("20.95");
-  });
+describe("Currency format", () => {
+  describe("formatCurrency", () => {
+    it("converts cents into dollors", () => {
+      expect(formatCurrency(2095)).toEqual("20.95");
+    });
 
-  it("works with 0", () => {
-    expect(formatCurrency(0)).toEqual("0.00");
-  });
+    it("works with 0", () => {
+      expect(formatCurrency(0)).toEqual("0.00");
+    });
 
-  it("rounds up to the nearest cent", () => {
-    expect(formatCurrency(2000.5)).toEqual("20.01");
-  });
+    it("rounds up to the nearest cent", () => {
+      expect(formatCurrency(2000.5)).toEqual("20.01");
+    });
 
-  it("rounds down to the nearest cent", () => {
-    expect(formatCurrency(2000.4)).toEqual("20.00");
-  });
+    it("rounds down to the nearest cent", () => {
+      expect(formatCurrency(2000.4)).toEqual("20.00");
+    });
 
-  it("checks for negative cents", () => {
-    expect(formatCurrency(-2000)).toEqual("-20.00");
+    it("checks for negative cents", () => {
+      expect(formatCurrency(-2000)).toEqual("-20.00");
+    });
   });
 });
