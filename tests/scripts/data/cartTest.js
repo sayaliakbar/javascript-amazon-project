7487028e3eb8c527fe920aa76109ce70caf3ab49
Ali Akbar <sayaliakbar@gmail.com>
2025-03-31 15:50:46 +0500

    Cart test updated to cart class test

--- tests/scripts/data/cartTest.js
+++ tests/scripts/data/cartTest.js
@@ -1,10 +1,4 @@
-import {
-  addToCart,
-  loadFromStorage,
-  cart,
-  removeFromCart,
-  updateDeliveryOption,
-} from "../../../scripts/data/cart.js";
+import { cart } from "../../../scripts/data/cart-class.js";
 
 /**
  * Test suite for the cart functionality
@@ -22,31 +16,28 @@ describe("Cart functionality", () => {
 
     it("adds an existing product to the cart", () => {
       // Mock localStorage.getItem to simulate cart with 1 product in storage
-      spyOn(localStorage, "getItem").and.callFake(() => {
-        return JSON.stringify([
-          {
-            productId: product1,
-            quantity: 1,
-            deliveryOptionId: "1",
-          },
-        ]);
-      });
-
-      // Initialize the cart from our mocked empty storage
-      loadFromStorage();
+      cart.cartItems = [
+        {
+          productId: product1,
+          quantity: 1,
+          deliveryOptionId: "1",
+        },
+      ];
 
       // Execute the function being tested with a specific product ID
-      addToCart("e43638ce-6aa0-4b85-b27f-e1d07eb678c6");
+      cart.addToCart("e43638ce-6aa0-4b85-b27f-e1d07eb678c6");
 
       // Assertions to verify correct behavior:
       // Verify the cart now contains exactly one item
-      expect(cart.length).toEqual(1);
+      expect(cart.cartItems.length).toEqual(1);
       // Verify the updated cart was saved to localStorage
       expect(localStorage.setItem).toHaveBeenCalledTimes(1);
       // Verify the correct product ID was added to the cart
-      expect(cart[0].productId).toEqual("e43638ce-6aa0-4b85-b27f-e1d07eb678c6");
+      expect(cart.cartItems[0].productId).toEqual(
+        "e43638ce-6aa0-4b85-b27f-e1d07eb678c6"
+      );
       // Verify the product was added with quantity of 1
-      expect(cart[0].quantity).toEqual(2);
+      expect(cart.cartItems[0].quantity).toEqual(2);
 
       expect(localStorage.setItem).toHaveBeenCalledWith(
         "cart",
@@ -62,25 +53,24 @@ describe("Cart functionality", () => {
 
     it("adds a new product to the cart", () => {
       // Mock localStorage.getItem to simulate an empty cart in storage
-      spyOn(localStorage, "getItem").and.callFake(() => {
-        return JSON.stringify([]);
-      });
+      cart.cartItems = [];
 
       // Initialize the cart from our mocked empty storage
-      loadFromStorage();
 
       // Execute the function being tested with a specific product ID
-      addToCart("e43638ce-6aa0-4b85-b27f-e1d07eb678c6");
+      cart.addToCart("e43638ce-6aa0-4b85-b27f-e1d07eb678c6");
 
       // Assertions to verify correct behavior:
       // Verify the cart now contains exactly one item
-      expect(cart.length).toEqual(1);
+      expect(cart.cartItems.length).toEqual(1);
       // Verify the updated cart was saved to localStorage
       expect(localStorage.setItem).toHaveBeenCalledTimes(1);
       // Verify the correct product ID was added to the cart
-      expect(cart[0].productId).toEqual("e43638ce-6aa0-4b85-b27f-e1d07eb678c6");
+      expect(cart.cartItems[0].productId).toEqual(
+        "e43638ce-6aa0-4b85-b27f-e1d07eb678c6"
+      );
       // Verify the product was added with quantity of 1
-      expect(cart[0].quantity).toEqual(1);
+      expect(cart.cartItems[0].quantity).toEqual(1);
       expect(localStorage.setItem).toHaveBeenCalledWith(
         "cart",
         JSON.stringify([
@@ -98,23 +88,19 @@ describe("Cart functionality", () => {
     beforeEach(() => {
       spyOn(localStorage, "setItem");
 
-      spyOn(localStorage, "getItem").and.callFake(() => {
-        return JSON.stringify([
-          {
-            productId: product1,
-            quantity: 1,
-            deliveryOptionId: "1",
-          },
-        ]);
-      });
-
-      loadFromStorage();
+      cart.cartItems = [
+        {
+          productId: product1,
+          quantity: 1,
+          deliveryOptionId: "1",
+        },
+      ];
     });
 
     it("removes a product from the cart", () => {
-      removeFromCart("e43638ce-6aa0-4b85-b27f-e1d07eb678c6");
+      cart.removeFromCart("e43638ce-6aa0-4b85-b27f-e1d07eb678c6");
 
-      expect(cart.length).toEqual(0);
+      expect(cart.cartItems.length).toEqual(0);
 
       expect(localStorage.setItem).toHaveBeenCalledTimes(1);
       expect(localStorage.setItem).toHaveBeenCalledWith(
@@ -124,9 +110,9 @@ describe("Cart functionality", () => {
     });
 
     it("removes a non existing product from the cart", () => {
-      removeFromCart("e43638ce-6aa0-4b85-b27f-abcdefghijkl");
+      cart.removeFromCart("e43638ce-6aa0-4b85-b27f-abcdefghijkl");
 
-      expect(cart.length).toEqual(1);
+      expect(cart.cartItems.length).toEqual(1);
       expect(localStorage.setItem).toHaveBeenCalledTimes(1);
       expect(localStorage.setItem).toHaveBeenCalledWith(
         "cart",
@@ -145,24 +131,20 @@ describe("Cart functionality", () => {
     beforeEach(() => {
       spyOn(localStorage, "setItem");
 
-      spyOn(localStorage, "getItem").and.callFake(() => {
-        return JSON.stringify([
-          {
-            productId: product1,
-            quantity: 1,
-            deliveryOptionId: "1",
-          },
-        ]);
-      });
-
-      loadFromStorage();
+      cart.cartItems = [
+        {
+          productId: product1,
+          quantity: 1,
+          deliveryOptionId: "1",
+        },
+      ];
     });
 
     it("update the delivery option of a product in the cart", () => {
-      updateDeliveryOption("e43638ce-6aa0-4b85-b27f-e1d07eb678c6", "3");
+      cart.updateDeliveryOption("e43638ce-6aa0-4b85-b27f-e1d07eb678c6", "3");
 
-      expect(cart.length).toEqual(1);
-      expect(cart[0].deliveryOptionId).toEqual("3");
+      expect(cart.cartItems.length).toEqual(1);
+      expect(cart.cartItems[0].deliveryOptionId).toEqual("3");
       expect(localStorage.setItem).toHaveBeenCalledTimes(1);
       expect(localStorage.setItem).toHaveBeenCalledWith(
         "cart",
@@ -177,18 +159,18 @@ describe("Cart functionality", () => {
     });
 
     it("edge case test: update the deliveryOptionId of a product that is not in the cart", () => {
-      updateDeliveryOption("e43638ce-6aa0-4b85-b27f-abcdefghij", "3");
+      cart.updateDeliveryOption("e43638ce-6aa0-4b85-b27f-abcdefghij", "3");
 
-      expect(cart.length).toEqual(1);
-      expect(cart[0].deliveryOptionId).toEqual("1");
+      expect(cart.cartItems.length).toEqual(1);
+      expect(cart.cartItems[0].deliveryOptionId).toEqual("1");
       expect(localStorage.setItem).toHaveBeenCalledTimes(0);
     });
 
     it("edge case test: use a deliveryOptionId that does not exist", () => {
-      updateDeliveryOption("e43638ce-6aa0-4b85-b27f-e1d07eb678c6", "4");
+      cart.updateDeliveryOption("e43638ce-6aa0-4b85-b27f-e1d07eb678c6", "4");
 
-      expect(cart.length).toEqual(1);
-      expect(cart[0].deliveryOptionId).toEqual("1");
+      expect(cart.cartItems.length).toEqual(1);
+      expect(cart.cartItems[0].deliveryOptionId).toEqual("1");
       expect(localStorage.setItem).toHaveBeenCalledTimes(0);
     });
   });
