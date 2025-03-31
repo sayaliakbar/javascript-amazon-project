3fd9878bc0fdda477df4b0817dac7323fa77bbc4
Ali Akbar <sayaliakbar@gmail.com>
2025-03-31 16:30:00 +0500

    Add backend practice file

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,9 @@
 import renderOrderSummary from "./checkout/orderSummary.js";
 import renderPaymentSummary from "./checkout/paymentSummary.js";
 import renderCheckoutHeader from "./checkout/checkoutHeader.js";
-// import "./data/car.js";
+import "./data/backend-practice.js";
+// import "./data/car.js"
+// import "./data/cart-oop.js";
 
 renderCheckoutHeader();
 renderOrderSummary();
