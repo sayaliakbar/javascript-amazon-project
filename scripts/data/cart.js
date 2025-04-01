7359fb631cae2c42b46fb4347b2db4f1a4ee774b
Ali Akbar <sayaliakbar@gmail.com>
2025-04-01 11:50:36 +0500

    Add asynchronous cart loading functionality with fetch and XMLHttpRequest

--- scripts/data/cart.js
+++ scripts/data/cart.js
@@ -163,3 +163,22 @@ export function updateDeliveryOption(productId, deliveryOptionId) {
 
   saveToLocalStorage();
 }
+async function loadCartFetch() {
+  const response = await fetch("https://supersimplebackend.dev/cart");
+
+  const result = await response.text();
+
+  console.log(result);
+}
+
+export function loadCart(fun) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener("load", () => {
+    console.log(xhr.response);
+    fun();
+  });
+
+  xhr.open("GET", "https://supersimplebackend.dev/cart");
+  xhr.send();
+}
