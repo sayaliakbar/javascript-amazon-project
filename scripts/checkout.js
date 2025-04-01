ee3c1b59e860d055bf89de711f91158c1546756d
Ali Akbar <sayaliakbar@gmail.com>
2025-04-01 12:36:27 +0500

    Remove unused loadPage function and related imports from checkout.js

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,31 +1,7 @@
 import renderOrderSummary from "./checkout/orderSummary.js";
 import renderPaymentSummary from "./checkout/paymentSummary.js";
 import renderCheckoutHeader from "./checkout/checkoutHeader.js";
-import { loadCart } from "./data/cart.js";
-import { loadProductsFetch } from "./data/products.js";
-// import "./data/backend-practice.js";
-// import "./data/car.js"
-// import "./data/cart-oop.js";
 
 renderCheckoutHeader();
 renderOrderSummary();
 renderPaymentSummary();
-
-async function loadPage() {
-  try {
-    await Promise.all([
-      loadProductsFetch(),
-      new Promise((resolve, reject) => {
-        loadCart(() => {
-          resolve("value3");
-        });
-      }),
-    ]);
-  } catch (error) {
-    console.log("Unexpected error. Please try again later.");
-  }
-
-  renderOrderSummary();
-  renderPaymentSummary();
-}
-loadPage();
