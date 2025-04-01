314a31923f012209e7be34efa210ba22019bd0a7
Ali Akbar <sayaliakbar@gmail.com>
2025-04-01 11:50:29 +0500

    Add asynchronous data loading for cart and products in checkout process

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,8 @@
 import renderOrderSummary from "./checkout/orderSummary.js";
 import renderPaymentSummary from "./checkout/paymentSummary.js";
 import renderCheckoutHeader from "./checkout/checkoutHeader.js";
+import { loadCart } from "./data/cart.js";
+import { loadProductsFetch } from "./data/products.js";
 // import "./data/backend-practice.js";
 // import "./data/car.js"
 // import "./data/cart-oop.js";
@@ -8,3 +10,22 @@ import renderCheckoutHeader from "./checkout/checkoutHeader.js";
 renderCheckoutHeader();
 renderOrderSummary();
 renderPaymentSummary();
+
+async function loadPage() {
+  try {
+    await Promise.all([
+      loadProductsFetch(),
+      new Promise((resolve, reject) => {
+        loadCart(() => {
+          resolve("value3");
+        });
+      }),
+    ]);
+  } catch (error) {
+    console.log("Unexpected error. Please try again later.");
+  }
+
+  renderOrderSummary();
+  renderPaymentSummary();
+}
+loadPage();
