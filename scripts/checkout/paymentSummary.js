8137d373fbbe6ac32a99b69b0f4ec23c48f874fe
Ali Akbar <sayaliakbar@gmail.com>
2025-04-02 14:42:19 +0500

    Update cart import path in paymentSummary.js

--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -1,4 +1,4 @@
-import { cart } from "../data/cart-class.js";
+import { cart } from "../data/cart.js";
 import { getProduct } from "../data/products.js";
 import { formatCurrency } from "../../utils/money.js";
 
